6c8fdbdf243e3379fd51f16b9c0dd0c7b076bfe9
leandrozmatias@gmail.com <leandrozmatias@gmail.com>
2022-12-14 00:27:31 +0000

    Básico de SELECT * FROM

---
+++ dia01/from.sql
@@ -0,0 +1,38 @@
+-- Databricks notebook source
+SELECT * FROM silver_olist.pedido
+
+-- leia-se: selecione TUDO da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT idPedido
+FROM silver_olist.pedido
+
+-- leia-se: selecione a coluna idPedido da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT idPedido,
+       descSituacao
+
+FROM silver_olist.pedido
+
+-- leia-se: selecione as colunas idPedido e descSituacao da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido,
+  descSituacao
+FROM
+  silver_olist.pedido
+LIMIT
+  5 -- leia-se: selecione 5 registros das colunas idPedido e descSituacao da tabela silver_olist.pedido
+
+-- COMMAND ----------
+
+SELECT
+  *,
+  DATEDIFF(dtEstimativaEntrega, dtEntregue) AS qtDiasPromessaEntrega
+FROM
+  silver_olist.pedido
